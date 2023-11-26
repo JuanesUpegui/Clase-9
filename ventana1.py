ec63355b25abb3a5d09e9d479942bf6fbbefba35
Santiago gomez <santihhhe@gmail.com>
2023-11-26 14:43:06 -0500

    Agrega los componentes de la ventana 1; fondo e iconos
    see: #1

--- ventana1.py
+++ ventana1.py
@@ -1,1 +1,72 @@
-# Codificar ventana 1
+import sys
+
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QLabel, QHBoxLayout, QApplication
+from PyQt5 import QtGui
+
+
+class Ventana1(QMainWindow):
+
+    # Metodo constructor de la ventana
+    def __init__(self,parent=None):
+        super(Ventana1, self).__init__(parent)
+
+        # poner el titulo
+        self.setWindowTitle("Formulario de registro")
+
+        # Ponemos el icono
+        self.setWindowIcon(QtGui.QIcon('imagenes/bag.png'))
+
+        # Establecemos las propiedades de ancho por alto
+        self.ancho = 900
+        self.alto = 600
+
+        # Establecemos el tamaño de la venata
+        self.resize(self.ancho, self.alto)
+
+        # Centrar la ventana en la pantalla
+        self.pantalla = self.frameGeometry()
+        self.centro = QDesktopWidget().availableGeometry().center()
+        self.pantalla.moveCenter(self.centro)
+        self.move(self.pantalla.topLeft())
+
+        # Fijar el tamaño de la ventana para evitar cambiarlo
+        self.setFixedWidth(self.ancho)
+        self.setFixedHeight(self.alto)
+
+        # Establecemos el fondo principal
+        self.fondo = QLabel(self)
+
+        # Definimos la imagen de fondo
+        self.imagenFondo = QPixmap('imagenes/OIP (2).jpeg')
+
+        # Definimos la iamgen de fondo
+        self.fondo.setPixmap(self.imagenFondo)
+
+        # Establecemos el modo para escalar la imagen
+        self.fondo.setScaledContents(True)
+
+        # Hacemos que se adapte al tamaño de la imagen
+        self.resize(self.imagenFondo.width(), self.imagenFondo.height())
+
+        # Establecemos la ventana de fondo como ventana central
+        self.setCentralWidget(self.fondo)
+
+        # Establecemos la distribucion de los elementos en layout horizontal
+        self.horizontal = QHBoxLayout()
+        # Le ponemos las margenes
+        self.horizontal.setContentsMargins(30, 30, 30, 30)
+
+        # PONER AL FINAL OJOOO
+        # indicamos que el layout principal del fondo es horizontal:
+        self.fondo.setLayout(self.horizontal)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    ventana1 = Ventana1()
+
+    ventana1.show()
+
+    sys.exit(app.exec_())
